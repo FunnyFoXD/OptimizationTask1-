89ff1604bd55a0480c1431b15275e242e5d3df6f
pet-z <pet_zed@rambler.ru>
2024-09-22 17:07:27 +0300

    I can read data in go

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
-import(
+import (
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	var n, m int
+	var e float64
+	fmt.Scan(&n, &m, &e)
+	c := make([]float64, n)   //objective function
+	a := make([][]float64, n) //coeficient matrix
+	b := make([]float64, n)   //right side
+	for i := 0; i < n; i++ {
+		a[i] = make([]float64, m)
+		fmt.Scan(&c[i])
+	}
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			fmt.Scan(&a[i][j])
+		}
+	}
+	for i := 0; i < n; i++ {
+		fmt.Scan(&b[i])
+	}
+	fmt.Scan(&e)
+
 }
